96330807832952bd73cb8d61e2332b05bc0d1713
Nyi Htut Lwin <lwinnyihtut0@gmail.com>
2023-04-23 16:43:48 +0630

    debug

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -19,7 +19,7 @@ const Homee = () => {
         isMobileInHome ? (<>
           <div className="flex flex-col md:flex-row items-center justify-center mt-8">
           <div className=" w-1/2 md:w-1/3 cursor-pointer">
-               <img src="/src/assets/image/ss_let.jpg" alt="" className=" rounded-Lg" data-aos="fade-up" data-aos-duration="1000"/>
+               <img src="src/assets/image/ss_let.jpg" alt="" className=" rounded-Lg" data-aos="fade-up" data-aos-duration="1000"/>
             </div>
             <div className="w-full md:w-1/2 px-4 mt-6">
               <span className=" text-2xl font-monument md:text-4xl text-primiray-text" data-aos="fade-right" data-aos-duration="2000">Choose</span>
