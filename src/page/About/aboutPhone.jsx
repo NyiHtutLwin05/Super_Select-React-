5cd07fece025e531dc7afefc301866a420a157f2
Nyi Htut Lwin <lwinnyihtut0@gmail.com>
2023-04-23 20:03:21 +0630

    bugged

--- src/page/About/aboutPhone.jsx
+++ src/page/About/aboutPhone.jsx
@@ -7,7 +7,7 @@ const AboutPhone = () => {
         <h1 className=" font-monument text-primiray-text text-2xl md:text-3xl items-center flex justify-center" data-aos="fade-down" data-aos-duration="1000"> About Us</h1>
         <div className=" mt-5">
           <div className=" flex items-center justify-center mt-10">
-          <img src='../assets/image/About/aboutMobile/CompanyImage.png' alt="" className=" w-[31%] md:w-[15%]"/>
+          <img src='src/assets/image/About/aboutMobile/CompanyImage.png' alt="" className=" w-[31%] md:w-[15%]"/>
            <h1 className=" font-monument md:text-3xl text-secondary-text w-32 ml-5 md:ml-9" data-aos="fade-up" data-aos-duration="1000">About Company</h1>
           </div>
           <div className=" font-sfPro flex justify-center mt-3">
@@ -20,7 +20,7 @@ const AboutPhone = () => {
         <div>
         <div className=" flex items-center justify-center mt-10">
            <h1 className=" font-monument md:text-3xl text-secondary-text w-32" data-aos="fade-down" data-aos-duration="2000">About Factory</h1>
-           <img src="/src/assets/image/About/aboutMobile/FactoryImage.png" alt="" className=" w-[31%] md:w-[15%] md:ml-10" data-aos="fade-right" data-aos-duration="1000"/>
+           <img src="src/assets/image/About/aboutMobile/FactoryImage.png" alt="" className=" w-[31%] md:w-[15%] md:ml-10" data-aos="fade-right" data-aos-duration="1000"/>
          
           </div>
           <div className=" font-sfPro flex justify-center mt-3 text-right">
