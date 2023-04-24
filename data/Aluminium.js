a86d1a8d3cca1256b0a0ffc1d6335e4783271b3e
Nyi Htut Lwin <lwinnyihtut0@gmail.com>
2023-04-24 10:09:23 +0630

    edited aluminium.js

--- data/Aluminium.js
+++ data/Aluminium.js
@@ -1,66 +1,79 @@
 
-
+import img1 from "../src/assets/image/Aluminium/IMG_0462.jpg";
+import img2 from "../src/assets/image/Aluminium/Image002.jpg";
+import img3 from "../src/assets/image/Aluminium/Image062.jpg";
+import img4 from "../src/assets/image/Aluminium/IMG_0462.jpg";
+import img5 from "../src/assets/image/Aluminium/IMG_0041.jpg";
+import img6 from "../src/assets/image/Aluminium/IMG_0225.jpg";
+import img7 from "../src/assets/image/Aluminium/A1.jpg";
+import img8 from "../src/assets/image/Aluminium/IMG_0444.jpg";
+import img9 from "../src/assets/image/Aluminium/IMG_0617.jpg";
+import img10 from "../src/assets/image/Aluminium/IMG_0708.jpg";
+import img11 from "../src/assets/image/Aluminium/IMG_0462.jpg";
+import img12 from "../src/assets/image/Aluminium/IMG_0733.jpg";
+import img13 from "../src/assets/image/Aluminium/IMG_20130403_135229.jpg";
+import img14 from "../src/assets/image/Aluminium/IMG_0624.jpg";
+import img15 from "../src/assets/image/Aluminium/IMG_0462.jpg";
 const AluData = [
-    {
-        "id":1,
-        "src": "/src/assets/image/Aluminium/Image002.jpg"
-      },
-      {
-        "id":2,
-        "src": "/src/assets/image/Aluminium/Image002.jpg"
-      },
-      {
-        "id":3,
-        "src": "/src/assets/image/Aluminium/Image062.jpg"
-      },
-      {
-        "id":4,
-        "src": "/src/assets/image/Aluminium/IMG_0041.JPG"
-      },
-      {
-        "id":5,
-        "src": "/src/assets/image/Aluminium/IMG_0225.JPG"
-      },
-      {
-        "id":6,
-        "src": "/src/assets/image/Aluminium/A1.jpg"
-      },
-      {
-        "id":7,
-        "src": "/src/assets/image/Aluminium/IMG_0444.JPG"
-      },
-      {
-        "id":8,
-        "src": "/src/assets/image/Aluminium/IMG_0592.JPG"
-      },
-      {
-        "id":9,
-        "src": "/src/assets/image/Aluminium/IMG_0617.JPG"
-      },
-      {
-        "id":10,
-        "src": "/src/assets/image/Aluminium/IMG_0708.JPG"
-      },
-      {
-        "id":11,
-        "src": "/src/assets/image/Aluminium/IMG_0733.JPG"
-      },
-      {
-        "id":12,
-        "src": "/src/assets/image/Aluminium/IMG_20130403_135229.jpg"
-      },
-      {
-        "id":13,
-        "src": "/src/assets/image/Aluminium/photo_957.jpg"
-      },
-      {
-        "id":14,
-        "src": "/src/assets/image/Aluminium/IMG_0624.JPG"
-      },
-      {
-        "id":15,
-        "src": "/src/assets/image/Aluminium/SAMSUNG_A8_013.jpg"
-      },
-
-]
+  {
+    id: 1,
+    src: img1,
+  },
+  {
+    id: 2,
+    src: img2,
+  },
+  {
+    id: 3,
+    src: img3,
+  },
+  {
+    id: 4,
+    src: img4,
+  },
+  {
+    id: 5,
+    src: img5,
+  },
+  {
+    id: 6,
+    src: img6,
+  },
+  {
+    id: 7,
+    src: img7,
+  },
+  {
+    id: 8,
+    src: img8,
+  },
+  {
+    id: 9,
+    src: img9,
+  },
+  {
+    id: 10,
+    src: img10,
+  },
+  {
+    id: 11,
+    src: img11,
+  },
+  {
+    id: 12,
+    src: img12,
+  },
+  {
+    id: 13,
+    src: img13,
+  },
+  {
+    id: 14,
+    src: img14,
+  },
+  {
+    id: 15,
+    src: img15,
+  },
+];
 export default AluData;
