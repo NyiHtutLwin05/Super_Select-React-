de030a4a78480d7fb94044e0b76566ca55f98707
Nyi Htut Lwin <lwinnyihtut0@gmail.com>
2023-05-09 10:10:36 +0630

    added some files

--- data/HandRail.js
+++ data/HandRail.js
@@ -1,80 +1,129 @@
-import H1 from '../src/assets/image/HandRail/H1.jpg'
-import H2 from '../src/assets/image/HandRail/H2.jpg'
-import H3 from '../src/assets/image/HandRail/H3.jpg'
-import H4 from '../src/assets/image/HandRail/H4.jpg'
-import H5 from '../src/assets/image/HandRail/H5.jpg'
-import H6 from '../src/assets/image/HandRail/H6.jpg'
-import H7 from '../src/assets/image/HandRail/H7.jpg'
-import H8 from '../src/assets/image/HandRail/H8.jpg'
-import H9 from '../src/assets/image/HandRail/H9.jpg'
-import H10 from '../src/assets/image/HandRail/H10.jpg'
-import H11 from '../src/assets/image/HandRail/H11.jpg'
-import H12 from '../src/assets/image/HandRail/H12.jpg'
-import H13 from '../src/assets/image/HandRail/H13.jpg'
-import H14 from '../src/assets/image/HandRail/H14.jpg'
-import H15 from '../src/assets/image/HandRail/H15.jpg'
-
 
 const HandData =[
     {
       'id':1,
-      src:H1,
+      src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR1.webp',
     },
     {
         'id':2,
-        src:H2,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR2.webp',
       },
       {
         'id':3,
-        src:H3,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR3.webp',
       },
       {
         'id':4,
-        src:H4,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR4.webp',
       },
       {
         'id':5,
-        src:H5,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR5.webp',
       },
       {
         'id':6,
-        src:H6,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR6.webp',
       },
       {
         'id':7,
-        src:H7,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR7.webp',
       },
       {
         'id':8,
-        src:H8,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR8.webp',
       },
       {
         'id':9,
-        src:H9,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR9.webp',
       },
       {
         'id':10,
-        src:H10,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR10.webp',
       },
       {
         'id':11,
-        src:H11,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR11.webp',
       },
       {
         'id':12,
-        src:H12,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR12.webp',
       },
       {
         'id':13,
-        src:H13,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR13.webp',
       },
       {
         'id':14,
-        src:H14,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR14.webp',
       },
       {
         'id':15,
-        src:H15,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR15.webp',
+      },
+      {
+        'id':16,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR16.webp',
+      },
+      {
+        'id':17,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR17.webp',
+      },
+      {
+        'id':18,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR18.webp',
+      },
+      {
+        'id':19,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR19.webp',
+      },
+      {
+        'id':20,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR20.webp',
+      },
+      {
+        'id':21,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR21.webp',
+      },
+      {
+        'id':22,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR22.webp',
+      },
+      {
+        'id':23,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR23.webp',
+      },
+      {
+        'id':24,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR24.webp',
+      },
+      {
+        'id':25,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR25.webp',
+      },
+      {
+        'id':26,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR26.webp',
+      },
+      {
+        'id':27,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR27.webp',
+      },
+      {
+        'id':28,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR28.webp',
+      },
+      {
+        'id':29,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR29.webp',
+      },
+      {
+        'id':30,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR30.webp',
+      },
+      
+      {
+        'id':31,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/HandRail/HR31.webp',
       },
 
 
