95596c58132b5ff35c78f68f3f1d4d7fa518efbf
Nyi Htut Lwin <lwinnyihtut0@gmail.com>
2023-05-11 14:45:56 +0630

    removed image folder

--- data/BathroomDoor.js
+++ data/BathroomDoor.js
@@ -1,114 +1,108 @@
-import Br1 from '../src/assets/image/BathRoomDoor/BR1.jpg'
-import Br2 from '../src/assets/image/BathRoomDoor/BR2.jpg'
-import Br3 from '../src/assets/image/BathRoomDoor/BR3.jpg'
-import Br4 from '../src/assets/image/BathRoomDoor/BR4.jpg'
-import Br5 from '../src/assets/image/BathRoomDoor/BR5.jpg'
-import Br6 from '../src/assets/image/BathRoomDoor/BR6.jpg'
-import Br7 from '../src/assets/image/BathRoomDoor/BR7.jpg'
-import Br8 from '../src/assets/image/BathRoomDoor/BR8.jpg'
-import Br9 from '../src/assets/image/BathRoomDoor/BR9.jpg'
-import Br10 from '../src/assets/image/BathRoomDoor/BR10.jpg'
-import Br11 from '../src/assets/image/BathRoomDoor/BR11.jpg'
-import Br12 from '../src/assets/image/BathRoomDoor/BR12.jpg'
-import Br13 from '../src/assets/image/BathRoomDoor/BR13.jpg'
-import Br14 from '../src/assets/image/BathRoomDoor/BR14.jpg'
-import Br15 from '../src/assets/image/BathRoomDoor/BR15.jpg'
-import Br16 from '../src/assets/image/BathRoomDoor/BR16.jpg'
-import Br17 from '../src/assets/image/BathRoomDoor/BR17.jpg'
-import Br18 from '../src/assets/image/BathRoomDoor/BR18.jpg'
-import Br19 from '../src/assets/image/BathRoomDoor/BR19.jpg'
-import Br20 from '../src/assets/image/BathRoomDoor/BR20.jpg'
-import Br21 from '../src/assets/image/BathRoomDoor/BR21.jpg'
-import Br22 from '../src/assets/image/BathRoomDoor/BR22.jpg'
+// import Br1 from '../src/assets/image/BathRoomDoor/BR1.jpg'
+// import Br2 from '../src/assets/image/BathRoomDoor/BR2.jpg'
+// import Br3 from '../src/assets/image/BathRoomDoor/BR3.jpg'
+// import Br4 from '../src/assets/image/BathRoomDoor/BR4.jpg'
+// import Br5 from '../src/assets/image/BathRoomDoor/BR5.jpg'
+// import Br6 from '../src/assets/image/BathRoomDoor/BR6.jpg'
+// import Br7 from '../src/assets/image/BathRoomDoor/BR7.jpg'
+// import Br8 from '../src/assets/image/BathRoomDoor/BR8.jpg'
+// import Br9 from '../src/assets/image/BathRoomDoor/BR9.jpg'
+// import Br10 from '../src/assets/image/BathRoomDoor/BR10.jpg'
+// import Br11 from '../src/assets/image/BathRoomDoor/BR11.jpg'
+// import Br12 from '../src/assets/image/BathRoomDoor/BR12.jpg'
+// import Br13 from '../src/assets/image/BathRoomDoor/BR13.jpg'
+// import Br14 from '../src/assets/image/BathRoomDoor/BR14.jpg'
+// import Br15 from '../src/assets/image/BathRoomDoor/BR15.jpg'
+// import Br16 from '../src/assets/image/BathRoomDoor/BR16.jpg'
+// import Br17 from '../src/assets/image/BathRoomDoor/BR17.jpg'
+// import Br18 from '../src/assets/image/BathRoomDoor/BR18.jpg'
+// import Br19 from '../src/assets/image/BathRoomDoor/BR19.jpg'
+// import Br20 from '../src/assets/image/BathRoomDoor/BR20.jpg'
+// import Br21 from '../src/assets/image/BathRoomDoor/BR21.jpg'
+// import Br22 from '../src/assets/image/BathRoomDoor/BR22.jpg'
 
 const Brdata=[
+ 
     {
         'id':1,
-        src:Br1
-    },
-    {
-        'id':0,
-        src:Br2,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD1.webp',
     },
     {
         'id':2,
-        src:Br3,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD2.webp',
     },
     {
         'id':3,
-        src:Br4
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD3.webp'
     },
     {
         'id':4,
-        src:Br5,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD4.webp',
     },
     {
         'id':5,
-        src:Br6,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD5.webp',
     },
     {
         'id':6,
-        src:Br7,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD6.webp',
     },
     {
         'id':7,
-        src:Br8,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD7.webp',
     },
     {
         'id':8,
-        src:Br9
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD8.webp'
     },
     {
         'id':9,
-        src:Br10
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD9.webp'
     },
     {
         'id':10,
-        src:Br11
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD10.webp'
     },
     {
         'id':11,
-        src:Br12,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD11.webp',
     },
     {
         'id':12,
-        src:Br13,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD12.webp',
     },
     {
         'id':13,
-        src:Br14,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD13.webp',
     },
     {
         'id':14,
-        src:Br15,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD14.webp',
     },
     {
         'id':15,
-        src:Br16,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD15.webp',
     },
     {
         'id':16,
-        src:Br17,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD16.webp',
     },
     {
         'id':17,
-        src:Br18,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD17.webp',
     },
     {
         'id':18,
-        src:Br19,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD18.webp',
     },
     {
         'id':19,
-        src:Br20,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD19.webp',
     },
     {
         'id':20,
-        src:Br21,
-    },
-    {
-        'id':21,
-        src:Br22,
+        src:'https://zxtodmdglvcilqtuxipr.supabase.co/storage/v1/object/public/SS_Image/Bathroom_Door/BD20.webp',
     }
+   
 ];
 export default Brdata;
